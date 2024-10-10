e932b0f2e9a6b6de1d93bc973d58c0b093e1c88a
ShaliniShukla <ShaliniShukla@CF-SHALINI_S>
2024-10-10 11:27:34 +0530

    Add project files.

---
+++ DigiDocumentation/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiDocumentation.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+            
+        }
+    }
+}
